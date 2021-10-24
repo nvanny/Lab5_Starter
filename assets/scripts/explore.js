0b88281dbe493de0a0d2b8ba52d73fb4f4e3b14a
Xi-Kai Wu <animal.wu@gmail.com>
2021-10-24 14:00:01 -0700

    select menu maybe working

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,13 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var synth = window.speechSynthesis;
+  var voices = synth.getVoices();
+  var menu = document.getElementById("voice-select");
+  for(var i = 0; i < voices.length; i++){
+    var opt = document.createElement("option");
+    opt.value = voices[i];
+    opt.innerHTML = voices[i];
+    menu.appendChild(opt);
+  }
 }
