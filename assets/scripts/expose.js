c04803df17c0ec6771f4693725ef8ab1fca14f76
nvanny <nvanny@ucsd.edu>
2021-10-24 02:32:07 -0700

    added img and audio switching

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,4 +4,19 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   // TODO
+  var elem = document.querySelector("select");
+  elem.addEventListener('change', function() {
+    var img = document.querySelector("img");
+    var audio = document.querySelector("audio");
+    var value = elem.options[elem.selectedIndex].value;
+    img.src = `assets/images/${value}.svg`;
+    audio.src = `assets/audio/${value}.mp3`;
+  });
+
+  var button = document.querySelector("button");
+  button.addEventListener('click', function() {
+    var audio = document.querySelector("audio");
+    audio.play();
+  });
 }
+
